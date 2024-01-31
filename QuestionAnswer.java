d944824953fce684d09d4b5d1c8146aa4688ddc6
Vaddi Govardhan <71170706+govardhan666@users.noreply.github.com>
2024-01-31 09:34:38 +0530

    Create QuestionAnswer.java

---
+++ QuestionAnswer.java
@@ -0,0 +1,25 @@
+package com.example.quiz_app;
+
+public class QuestionAnswer {
+
+    public static String question []={
+            "What is 10+26 ?",
+            "Who invented Telephone?",
+            "what is 12*9 ?",
+            "who is the founder of SpaceX?",
+            "In the given options, which is the Example of System Software?"
+    };
+    public static String choices [][]={
+            {"32" , "42" , "36" , "38"},
+            {"Graham Bell" , "Einstein" , "Edison" , "None of the above"},
+            {"96" , "84" , "102" , "108"},
+            {"Jeff Bezos" , "Elon Musk" , "Steve Jobs" , "Bill Gates"},
+            {"Windows" , "Linux" , "MacOS" , "All of the above"}
+    };
+    public static String correctAnswers []={
+            "36" ,
+            "Graham Bell",
+            "108",
+            "Elon Musk",
+            "All of the above"};
+}
